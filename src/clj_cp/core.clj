aae7d135800ebe3bb6a434ac80229d24e62a249a
Alex Engelberg <alex.benjamin.engelberg@gmail.com>
2013-10-16 18:26:15 -0700

    Added a "SolverState" mechanism allowing built-in functions to attach information to the solver for later

--- src/clj_cp/core.clj
+++ src/clj_cp/core.clj
@@ -12,8 +12,29 @@
   [name]
   (solver.Solver. name))
 
+(defrecord ^:private SolverState
+  [solver n-solutions my-vars])
+
+(defn- new-solver-state
+  [solver]
+  (->SolverState solver (atom 0) (atom {})))
+
+(def ^:private ^:dynamic current-solver-state-binding nil)
+(defonce ^:private current-solver-state-atom (atom nil))
+
+(defn- get-current-solver-state []
+  (or @current-solver-state-atom
+      current-solver-state-binding
+      (throw (Exception. "No \"solver\" binding found, try using with-solver"))))
+(defn get-current-solver []
+  (:solver (get-current-solver-state)))
+(defn- get-current-solution-n-atom []
+  (:n-solutions (get-current-solver-state)))
+(defn- get-current-vars-atom []
+  (:my-vars (get-current-solver-state)))
+
 (def ^:private ^:dynamic current-solver-binding nil)
-(def ^:private current-solver-atom (atom nil))
+(defonce ^:private current-solver-atom (atom nil))
 
 (defmacro with-solver
   "A way of setting the current solver, using bindings.
@@ -23,7 +44,7 @@ Syntax:
 Note that, because bindings are in play behind the scenes, you can separate solver-dependent function calls into different functions as long as they are eventually nested in the initial bind.
 See \"with-solver!\" for a more unsafe but convenient way to permanently set the solver binding without enclosing an expression."
   [solver & exprs]
-  `(binding [current-solver-binding ~solver]
+  `(binding [current-solver-state-binding (new-solver-state ~solver)]
      ~@exprs))
 
 (defn with-solver!
@@ -35,12 +56,7 @@ Syntax:
 (with-solver! <my-solver>)
 ..."
   [solver]
-  (reset! current-solver-atom solver))
-
-(defn get-current-solver []
-  (or @current-solver-atom
-      current-solver-binding
-      (throw (Exception. "No \"solver\" binding found, try using with-solver"))))
+  (reset! current-solver-atom (new-solver-state solver)))
 
 (defn int-var
   "Creates an integer variable with the desired starting domain.
@@ -57,15 +73,17 @@ A bounded var only stores the min and max of the domain interval."
         [domain-expr args] (if (number? (first args))
                              [{:min (first args) :max (second args)} (rest (rest args))]
                              [(first args) (rest args)])
-        domain-type (or (first args) :enumerated)]
-    (case [domain-type (sequential? domain-expr)]
-      [:enumerated false] (VF/enumerated name (:min domain-expr) (:max domain-expr)
+        domain-type (or (first args) :enumerated)
+        v (case [domain-type (sequential? domain-expr)]
+            [:enumerated false] (VF/enumerated name (:min domain-expr) (:max domain-expr)
+                                               (get-current-solver))
+            [:enumerated true] (VF/enumerated name (int-array domain-expr)
+                                              (get-current-solver))
+            [:bounded false] (VF/bounded name (:min domain-expr) (:max domain-expr)
                                          (get-current-solver))
-      [:enumerated true] (VF/enumerated name (int-array domain-expr)
-                                        (get-current-solver))
-      [:bounded false] (VF/bounded name (:min domain-expr) (:max domain-expr)
-                                   (get-current-solver))
-      [:bounded true] (throw (Exception. "Bounded int-vars only take a min and a max")))))
+            [:bounded true] (throw (Exception. "Bounded int-vars only take a min and a max")))]
+    (swap! (get-current-vars-atom) update-in [:int] conj v)
+    v))
 
 (defn const-var
   "Takes a number, and creates an object that behaves like an int-var but is in fact a constant number. The variable will not be included in a solution map.
@@ -85,34 +103,25 @@ Useful when using constraints that require a variable instead of a constant."
   (let [[name? args] (if (namey? (first args))
                        [(first args) (rest args)]
                        [nil args])
-        name (name (if name? name? (gensym "boolvar")))]  ; beware the funky usage of "name"
-    (VF/bool name (get-current-solver))))
+        name (name (if name? name? (gensym "boolvar")))  ; beware the funky usage of "name"
+        v (VF/bool name (get-current-solver))]
+    (swap! (get-current-vars-atom) update-in [:bool] conj v)
+    v))
 
 (alter-meta! #'bool-var assoc :arglists '([name?]))
 
 (defn get-val
+  "Returns the one and only value in a variable's domain (otherwise the min, if there are multiple values)."
   [variable]
   (.getValue variable))
 
 (defn- solution-map
   [solver n]
   (into {:solution n}
-        (for [v (.getVars solver)
+        (for [v (apply concat (vals @(get-current-vars-atom)))
               :let [n (.getName v)]
               :when (not= (first n) \_)]
           [n (.getValue v)])))
-
-(defn- lazy-solutions
-  ([solver]
-    (if (.findSolution solver)
-      (cons (solution-map solver 0)
-            (lazy-seq (lazy-solutions solver 1)))
-      ()))
-  ([solver n]
-    (if (.nextSolution solver)
-      (cons (solution-map solver n)
-            (lazy-seq (lazy-solutions solver (inc n))))
-      ())))
 
 (defn constrain!
   "Enforces a constraint to be true when it comes time to solve the variables.
@@ -132,19 +141,20 @@ A useful idiom for imperatively iterating through all the solutions:
   <do stuff with variables>)"
   []
   (let [solver (get-current-solver)]
-    (try (.findSolution solver)
+    (try (do (.findSolution solver)
+           (swap! (get-current-solution-n-atom) inc))
       (catch SolverException e
         (.nextSolution solver)))))
 
 (defn solution
-  "Solves the solver using the posted constraints and returns a map from variable names to their values. You shouldn't call this function more than once."
+  "Solves the solver using the posted constraints and returns a map from variable names to their values (or false if there is no solution).
+You can call this function more than once, getting a new solution each time (like \"solve!\")."
   []
   (let [solver (get-current-solver)]
-    (do (.findSolution solver)
-      (solution-map 0))))
+    (and (solve!)
+         (solution-map (dec @(get-current-solution-n-atom))))))
 
 (defn solutions
   "Solves the solver using the posted constraints and returns a lazy seq of maps (for each solution) from variable names to their values. You shouldn't call this function more than once."
   []
-  (let [solver (get-current-solver)]
-    (lazy-solutions solver)))
+  (take-while identity (repeatedly solution)))
