ccd31e23598962cecc10eaa470b780ba09f77d7a
Alex Engelberg <alex.benjamin.engelberg@gmail.com>
2015-04-18 10:29:41 -0700

    Changed most of the test cases to use a new macro for readability

--- test/loco/core_test.clj
+++ test/loco/core_test.clj
@@ -6,125 +6,114 @@
 (defmacro test-constraint-model
   ([docstring model solution-maps]
    `(testing ~docstring
-      (is (= (set ~solution-maps)
+      (is (= ~(set solution-maps)
              (set (solutions ~model))))))
   ([model solution-maps]
    `(test-constraint-model nil ~model ~solution-maps)))
 
 (deftest basic-test
-  (-> (solutions
-        [($in :x 1 3)
-         ($in :y 1 3 :bounded)
-         ($in :z [1 3 2])
-         ($= :x :y)
-         ($= :y :z)])
-    (= '({:x 1 :y 1 :z 1} {:x 2 :y 2 :z 2} {:x 3 :y 3 :z 3}))
-    is))
+  (test-constraint-model
+   "Basic test case with $= and bounded vars "
+   [($in :x 1 3)
+    ($in :y 1 3 :bounded)
+    ($in :z [1 3 2])
+    ($= :x :y)
+    ($= :y :z)]
+   [{:x 1 :y 1 :z 1} {:x 2 :y 2 :z 2} {:x 3 :y 3 :z 3}]))
 
 (deftest arithmetic-test
-  (-> (solutions
-        [($in :x -5 5)
-         ($in :y -5 5)
-         ($in :z -5 5)
-         ($= ($+ :x :y) 5)
-         ($= ($- :x :z) 2)
-         ($= ($* :y :z) 2)])
-    set
-    (= #{{:z 1, :y 2, :x 3} {:z 2, :y 1, :x 4}})
-    is))
+  (test-constraint-model
+   [($in :x -5 5)
+    ($in :y -5 5)
+    ($in :z -5 5)
+    ($= ($+ :x :y) 5)
+    ($= ($- :x :z) 2)
+    ($= ($* :y :z) 2)]
+   [{:z 1, :y 2, :x 3} {:z 2, :y 1, :x 4}]))
 
 (deftest abs-test
-  (-> (solutions
-        [($in :x -5 5)
-         ($= ($abs :x) 2)])
-    set
-    (= #{{:x -2} {:x 2}})
-    is))
+  (test-constraint-model
+   [($in :x -5 5)
+    ($= ($abs :x) 2)]
+   [{:x -2} {:x 2}]))
 
 (deftest minmax-test
-  (-> (solutions
-        [($in :x -5 5)
-         ($in :y -5 5)
-         ($in :z -5 5)
-         ($= ($min :x :y :z) :x)
-         ($= ($max :x :y :z) :z)
-         ($= :x -5)
-         ($= :z -5)])
-    (= '({:x -5 :y -5 :z -5}))
-    is)
-  (-> (solutions
-        [($in :x 1 5)
-         ($in :y 2 6)
-         ($in :z 3 7)
-         ($= ($min :x 5) 5)
-         ($= ($max :z 3) 3)
-         ($= :x :y)])
-    (= '({:x 5 :z 3 :y 5}))
-    is))
+  (test-constraint-model
+   [($in :x -5 5)
+    ($in :y -5 5)
+    ($in :z -5 5)
+    ($= ($min :x :y :z) :x)
+    ($= ($max :x :y :z) :z)
+    ($= :x -5)
+    ($= :z -5)]
+   [{:x -5 :y -5 :z -5}])
+  (test-constraint-model
+   [($in :x 1 5)
+    ($in :y 2 6)
+    ($in :z 3 7)
+    ($= ($min :x 5) 5)
+    ($= ($max :z 3) 3)
+    ($= :x :y)]
+   [{:x 5 :z 3 :y 5}]))
 
 (deftest mod-scalar-test
-  (-> (solutions
-        [($in :x 1 5)
-         ($in :y 1 5)
-         ($in :z 1 5)
-         ($= ($mod :x :y) 4)
-         ($= ($scalar [:x :y :z] '(1 1 -2)) 3)])
-    (= '({:x 4 :y 5 :z 3}))
-    is))
+  (test-constraint-model
+   [($in :x 1 5)
+    ($in :y 1 5)
+    ($in :z 1 5)
+    ($= ($mod :x :y) 4)
+    ($= ($scalar [:x :y :z] '(1 1 -2)) 3)]
+   [{:x 4 :y 5 :z 3}]))
 
 (deftest eq-ineq-test
-  (-> (solutions
-        [($in :x 1 5)
-         ($in :y 1 5)
-         ($in :z 1 5)
-         ($= :z 2)
-         ($< :x :y)
-         ($<= :y :z)
-         ($> :y :x)
-         ($>= :z :y)
-         ($!= :x :y)
-         ($!= :x :y :z)])
-    (= '({:x 1 :y 2 :z 2}))
-    is))
+  (test-constraint-model
+   [($in :x 1 5)
+    ($in :y 1 5)
+    ($in :z 1 5)
+    ($= :z 2)
+    ($< :x :y)
+    ($<= :y :z)
+    ($> :y :x)
+    ($>= :z :y)
+    ($!= :x :y)
+    ($!= :x :y :z)]
+   [{:x 1 :y 2 :z 2}]))
 
 (deftest logic-test
-  (-> (solutions
-        [($in :x [1])
-         ($true)
-         ($not ($false))
-         ($not ($not ($true)))
-         ($and ($true) ($true))
-         ($not ($and ($true) ($false)))
-         ($or ($true) ($false))
-         ($if ($true) ($true) ($false))
-         ($if ($false) ($false))
-         ($if ($false) ($false) ($true))
-         ($cond
-           ($false) ($true)
-           ($false) ($false)
-           ($true) ($true)
-           ($false) ($true)
-           :else ($true))])
-    (= '({:x 1}))
-    is))
+  (test-constraint-model
+   [($in :x [1])
+    ($true)
+    ($not ($false))
+    ($not ($not ($true)))
+    ($and ($true) ($true))
+    ($not ($and ($true) ($false)))
+    ($or ($true) ($false))
+    ($if ($true) ($true) ($false))
+    ($if ($false) ($false))
+    ($if ($false) ($false) ($true))
+    ($cond
+     ($false) ($true)
+     ($false) ($false)
+     ($true) ($true)
+     ($false) ($true)
+     :else ($true))]
+    [{:x 1}]))
 
 (deftest reify-test
-  (-> (solutions
-        [($in :x 0 1)
-         ($= ($reify ($true)) :x)
-         ($= ($reify ($false)) ($- 1 :x))])
-    (= '({:x 1}))
-    is))
+  (test-constraint-model
+   [($in :x 0 1)
+    ($= ($reify ($true)) :x)
+    ($= ($reify ($false)) ($- 1 :x))]
+   [{:x 1}]))
 
 (deftest all-different-test
-  (-> (solutions
-        [($in :x 0 1)
-         ($in :y [1])
-         ($in :z 1 2)
-         ($distinct [:x :y :z])
-         ($not ($distinct [:x :x]))])
-    (= '({:x 0 :y 1 :z 2}))
-    is))
+  (test-constraint-model
+   [($in :x 0 1)
+    ($in :y [1])
+    ($in :z 1 2)
+    ($distinct [:x :y :z])
+    ($not ($distinct [:x :x]))]
+   [{:x 0 :y 1 :z 2}]))
 
 (deftest circuit-test
   (-> (solution
@@ -166,28 +155,24 @@
             (is (= (count (distinct [v w x y z])) 5))))))
 
 (deftest nth-test
-  (-> (solutions
-        [($in :a [5])
-         ($in :b [5])
-         ($in :c [2])
-         ($in :d [5])
-         ($in :e [5])
-         ($in :x 0 4)
-         ($= ($nth [:a :b :c :d :e] :x) :x)])
-    (as-> s (and (= (count s) 1)
-                 (= ((first s) :x) 2)))
-    is)
-  (-> (solutions
-        [($in :a [5])
-         ($in :b [5])
-         ($in :c [3])
-         ($in :d [5])
-         ($in :e [5])
-         ($in :x 0 4)
-         ($= ($nth [:a :b :c :d :e] :x 1) :x)])
-    (as-> s (and (= (count s) 1)
-                 (= ((first s) :x) 3)))
-    is))
+  (test-constraint-model
+   [($in :a [5])
+    ($in :b [5])
+    ($in :c [2])
+    ($in :d [5])
+    ($in :e [5])
+    ($in :x 0 4)
+    ($= ($nth [:a :b :c :d :e] :x) :x)]
+   [{:a 5 :b 5 :c 2 :d 5 :e 5 :x 2}])
+  (test-constraint-model
+   [($in :a [5])
+    ($in :b [5])
+    ($in :c [3])
+    ($in :d [5])
+    ($in :e [5])
+    ($in :x 0 4)
+    ($= ($nth [:a :b :c :d :e] :x 1) :x)]
+   [{:a 5 :b 5 :c 3 :d 5 :e 5 :x 3}]))
 
 (deftest automaton-test
   (let [regex "(1|2)3*(4|5)"]
@@ -238,19 +223,18 @@
          nil)))
 
 (deftest tricky-test-0-2-1
-  ; testing ($comparison number var)
-  (-> (solutions
-        [($in :a [5])
-         ($in :b 0 1)
-         ($= :b ($reify ($< 0 :a)))])
-    (= '({:a 5 :b 1}))
-    is))
+  "Target specific bug fixed by 0.2.1"
+  (test-constraint-model
+   [($in :a [5])
+    ($in :b 0 1)
+    ($= :b ($reify ($< 0 :a)))]
+   [{:a 5 :b 1}]))
 
 (deftest knapsack-test
   (test-constraint-model
    "Super basic knapsack constraint"
    [($knapsack [1 1] [1 1] [1 1] 2 2)]
-   '({}))
+   [{}])
   (test-constraint-model
    "Basic knapsack constraint"
    [($knapsack [2 3]
@@ -262,13 +246,13 @@
     ($in :y 0 10)
     ($in :total-weight 0 10)
     ($in :total-value 0 10)]
-   '({:x 0 :y 0 :total-weight 0 :total-value 0}
-     {:x 1 :y 0 :total-weight 2 :total-value 6}
-     {:x 0 :y 1 :total-weight 3 :total-value 5}
-     {:x 0 :y 2 :total-weight 6 :total-value 10}))
+   [{:x 0 :y 0 :total-weight 0 :total-value 0}
+    {:x 1 :y 0 :total-weight 2 :total-value 6}
+    {:x 0 :y 1 :total-weight 3 :total-value 5}
+    {:x 0 :y 2 :total-weight 6 :total-value 10}])
   (test-constraint-model
    "Knapsack constraint with negatives"
    [($in :x -100 100)
     ($in :y -100 100)
     ($knapsack [1 1] [10 20] [:x :y] 10 10)]
-   '({:x 19 :y -9})))
+   [{:x 19 :y -9}]))
