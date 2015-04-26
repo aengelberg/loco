87a1a753fb538decba2b9901e3e24187bebc8c61
Andrea Richiardi <a.richiardi.work@gmail.com>
2015-04-26 21:02:39 +0200

    Implemented Magic Series solution that uses $cardinality (#1) while solution #2 takes the long way  of explicitely calculating the occurrences constraints.

--- test/loco/integer/magicseries.clj
+++ test/loco/integer/magicseries.clj
@@ -1,75 +1,59 @@
 (ns ^{:author "Andrea Richiardi"
-      :doc "Magic Series example + tests.
+      :doc "Magic Series example + tests. It makes use of loco's $cardinality global constraint.
             A series S = (S0,...Sn) is magic if Si represents the number of occurrences of i in S.
-            Example: A Magic Series of lenght 4 is |2|1|2|0|0|"}
+            For example a Magic Series of length 5 is |2|1|2|0|0|"}
   loco.integer.magicseries
   (:require [clojure.test :refer :all]
             [loco.core :refer :all]
-            [loco.constraints :refer :all]
-            [criterium.core :refer [bench]]))
+            [loco.constraints :refer :all]))
 
 (defn series-vars
   "Returns the control variables of the Magic Series."
-  [series-lenght]
-  (for [i (range series-lenght)]
+  [series-length]
+  (for [i (range series-length)]
     [:s i]))
 
 (defn numeric-constraints
   "Builds the numeric constraints of each Magic Series control variable
   in input."
   [series]
-  (let [series-lenght (count series)]
+  (let [series-length (count series)]
     (for [s series] 
-      ($in s 0 (dec series-lenght)))))
+      ($in s 0 (dec series-length)))))
 
-(defn occurrence-count
-  "Given a k element in the Magic Series, returns a var containing the
-  number of its occurrences in the series itself."
-  [i series]
-  (apply $+ (for [s series]
-              ($reify ($= i s)))))
-
-(defn occurrence-constraints
-  "Builds the occurrence constraint of each Magic Series control variable
-  in input."
+(defn frequency-map
+  "Builds the frequency map of the series in input for the $cardinality
+  constraint."
   [series]
-  (for [s series]
-    ($= s (occurrence-count (second s) series))))
+  (into {} (for [[k i] series] [i [k i]])))
 
 (defn ms-model
   "Return the model."
-  [series-lenght]
-  (let [series (series-vars series-lenght)] 
-    (concat series
-            (numeric-constraints series)
-            (occurrence-constraints series))))
+  [series-length]
+  (let [series (series-vars series-length)] 
+    (conj (concat series (numeric-constraints series)) 
+          ($cardinality series (frequency-map series)))))
 
 (defn reorder-solution
   "Manages to rearrange a solution in a better way."
   [solution]
   (into (sorted-map) solution))
 
-(defn random-solution
-  "Returns a random-picked solution, some gobbledygook was necessary to
-  rearrange it."
-  [series-lenght]
-  (if-let [sols (seq (solutions (ms-model series-lenght)))]
-    (rand-nth sols)
-    ()))
+(deftest magicseries-tests
+  "Testing Magic Series solution #1. One solution given a length."
 
-(def bench-solution
-  #(bench (solution ms-model)))
+  ;; No solution for length < 4
+  (is (empty? (solution (ms-model 1))) "Length 1 does not have solution")
+  (is (empty? (solution (ms-model 2))) "Length 2 does not have solution")
+  (is (empty? (solution (ms-model 3))) "Length 3 does not have solution")
 
-(def bench-solutions
-  #(bench (solutions ms-model)))
-
-(deftest magicseries
-  "Testing Magic Series solution"
-
-  ;; One solution per model given a length, testing some random length
-  (doseq [l (take 10 (repeatedly #(rand-int 50)))]
+  ;; No solution for length = 6 (try it out!)
+  (is (empty? (solution (ms-model 6))) "Length 6 does not have solution")
+  
+  ;; Testing between 4 (inclusive) and 50 (inclusive)
+  (doseq [l (take 10 (repeatedly #(+ 4 (rand-int 47))))]
     (let [sol (solution (ms-model l))] 
-      (println "Solution for length:" l " " (reorder-solution sol)) 
-        
+      #_(println "Solution for length:" l " " (reorder-solution sol)) 
+
       (doseq [[[k i] occ] (seq sol)]
         (is (= occ (count (filter #(= i %1) (map second sol)))))))))
