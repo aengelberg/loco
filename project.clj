8620414c6666052fd20df43b9641e72a6acaa1d7
Alex Engelberg <alex.benjamin.engelberg@gmail.com>
2016-05-01 19:58:25 -0700

    Bump choco version to latest

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.choco-solver/choco-solver "3.3.0"]])
+                 [org.choco-solver/choco-solver "3.3.3"]])
